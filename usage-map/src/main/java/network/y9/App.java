1211fbb55f7b151181d36591feee69ea9b7ca6b6
moryxsw <moryxsw@outlook.com>
2023-09-28 00:07:59 +0800

    feat:添加map移除方法

--- usage-map/src/main/java/network/y9/App.java
+++ usage-map/src/main/java/network/y9/App.java
@@ -1,9 +1,8 @@
 package network.y9;
 
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArraySet;
 import java.util.stream.Collectors;
 
 /**
@@ -12,6 +11,12 @@ import java.util.stream.Collectors;
  */
 public class App {
     public static void main( String[] args ) {
+
+    }
+
+
+
+    public void sort(){
         HashMap<String, Integer> map = new HashMap<>(){{
             put("a", 1);
         }};
@@ -45,5 +50,52 @@ public class App {
         // treemap排序
         Map<String, Integer> sorted3 = new TreeMap<>(code);
         System.out.println(sorted3);
+    }
+
+
+    /**
+     * 根据value删除map中的元素
+     */
+    public void delByValue(){
+        Map<String, String> init = new HashMap<>();
+        init.put("1","01");
+
+
+        // 1. hashMap 使用的是fail fast错误机制，所以使用CopyOnWriteArraySet保障线程安全
+        Set<Map.Entry<String, String>> entries = new CopyOnWriteArraySet<>(init.entrySet());
+        for (Map.Entry<String, String> entry : entries) {
+            if("01".equals(entry.getValue())){
+                init.remove(entry.getKey());
+            }
+        }
+
+
+        // foreach
+        ConcurrentHashMap<String, String> map = new ConcurrentHashMap<>(init);
+        map.forEach((k,v)->{
+            if("01".equals(v)){
+                map.remove(k);
+            }
+        });
+
+        // 迭代器
+        Iterator<Map.Entry<String, String>> iterator = init.entrySet().iterator();
+        while (iterator.hasNext()){
+            Map.Entry<String, String> next = iterator.next();
+            if("01".equals(next.getValue())){
+                iterator.remove();
+            }
+        }
+
+        // remove if
+
+        init.entrySet().removeIf(entry -> "01".equals(entry.getValue()));
+
+
+        // stream
+        init = init.entrySet().stream().filter(entry -> !"01".equals(entry.getValue()))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+
+
     }
 }
